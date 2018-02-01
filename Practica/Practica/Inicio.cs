da8be764bca137e370cf9401a0d3465b75c49026
punts hola mundo <anthonnyc10@gmail.com>
2018-02-01 13:32:33 -0600

    Metodo de insertar usuarios a la base de datos

---
+++ Practica/Practica/Inicio.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Practica
+{
+    public partial class Registro : Form
+    {
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+
+        public Registro()
+        {
+            InitializeComponent();
+        }
+
+        private void bntContraseña_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Registro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(329, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "USUARIO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(329, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CONTRASEÑA:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(347, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(310, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SISTEMA DE REGISTRO DE VENTAS";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(497, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(497, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(518, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 31);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "INGRESAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Registro
+            // 
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(987, 475);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registro";
+            this.Text = "GOLOSINAS SALAZAR";
+            this.Load += new System.EventHandler(this.Registro_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Registro_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Practica/capa_datos/Conexiones_Bases_Datos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace capa_datos
+{
+    public class Conexiones_Bases_Datos
+    {
+
+        static NpgsqlConnection conexion;
+        static NpgsqlCommand cmd;
+
+        public static void Conectando_Base_Datos()
+        {
+            string servidor = "localhost";
+            int puerto = 5432;
+            string usuario = "postgres";
+            string clave = "1414250816ma";
+            string baseDatos = "sistema_ventas";
+
+            string cadenaConexion = "Server=" + servidor + ";" + "Port=" + puerto + ";" + "User Id=" + usuario + ";" + "Password=" + clave + ";" + "Database=" + baseDatos;
+            conexion = new NpgsqlConnection(cadenaConexion);
+        }
+
+
+        //Esta funcion inserta datos en la tabla usuarios 
+        public void InsertarDatosUsuario(int contraseña, string usuario)
+        {
+            Conectando_Base_Datos();
+            conexion.Open();
+            cmd = new NpgsqlCommand("INSERT INTO usuario (contraseña, usuario, edad, comentario) VALUES ('" + contraseña + "', '" + usuario +  "')", conexion);
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+
+
+
+
+
+
+    }
+}
