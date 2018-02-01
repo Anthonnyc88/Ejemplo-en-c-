d1620dbc10227a57b6a22d47d323972a48a2985b
punts hola mundo <anthonnyc10@gmail.com>
2018-02-01 13:54:46 -0600

    Se realizo los metodos de insertar productos , eliminar y modificar listos

--- Practica/capa_datos/Conexiones_Bases_Datos.cs
+++ Practica/capa_datos/Conexiones_Bases_Datos.cs
@@ -31,16 +31,57 @@ namespace capa_datos
         {
             Conectando_Base_Datos();
             conexion.Open();
-            cmd = new NpgsqlCommand("INSERT INTO usuario (contraseña, usuario, edad, comentario) VALUES ('" + contraseña + "', '" + usuario +  "')", conexion);
+            cmd = new NpgsqlCommand("INSERT INTO usuario (contraseña, usuario, ) VALUES ('" + contraseña + "', '" + usuario +  "')", conexion);
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+        //Esta funcion modifica la contraseña guardada en la tabla de la bases de datos
+        public void ModificarContraseñaUsuario(int contraseña, string usuario)
+        {
+            Conectando_Base_Datos();
+            conexion.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("UPDATE usuario SET usuario = '" + usuario  + "' WHERE contraseña = '" + contraseña + "'", conexion);
             cmd.ExecuteNonQuery();
             conexion.Close();
         }
 
 
 
+        //Este metodo inserta productos hacia la base de datos
+        public void InsertarDatosProducto(int codigo, string nombre, int cantidad , string proveedor , string fecha_registro)
+        {
+            Conectando_Base_Datos();
+            conexion.Open();
+            cmd = new NpgsqlCommand("INSERT INTO producto (codigo, nombre, cantidad, proveedor , fecha_registro ) VALUES ('" + codigo + "', '" + nombre + "', '" + cantidad + "', '" + proveedor + "', '" + fecha_registro + "')", conexion);
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
 
 
+        //Este metodo modifica la tabla productos dentro de la base de datos
+        public void ModificarDatosProductos(int codigo, string nombre, int cantidad, string proveedor , string fecha_registro)
+        {
+            Conectando_Base_Datos();
+            conexion.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("UPDATE producto SET nombre = '" + nombre + "', cantidad = '" + cantidad + "', proveedor = '" + proveedor + "', fecha_registro = '" + fecha_registro + "' WHERE codigo = '" + codigo + "'", conexion);
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
 
-
+        //Este metodo elimina un dato dentro de la base de datos 
+        public void EliminarDatosProductos(int codigo)
+        {
+            Conectando_Base_Datos();
+            conexion.Open();
+            NpgsqlCommand cmd = new NpgsqlCommand("DELETE FROM producto WHERE codigo = '" + codigo + "'", conexion);
+            cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
     }
+
+
+
 }
+
+
